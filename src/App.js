bf8dfb4b97bf7628b13469cc5303d97ac767d582
Rudie G <rauoldm@gmail.com>
2021-08-14 13:46:23 -0500

    forgot to commit and finished the app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import RgbSlider from './components/RgbSlider'
 import RgbDisplay from './components/RgbDisplay'
-// Bring information from all three rgbsliders, assign to respective colors in the RgbDisplay
+// hring information from all three rgbsliders, assign to respective colors in the RgbDisplay
 
 function App() {
   const [ colorRed, setColorRed ] = useState(0);
   const [ colorBlue, setColorBlue ] = useState(0);
   const [ colorGreen, setColorGreen ] = useState(0);
+  let red = "Red";
+  let blue = "Blue";
+  let green = "Green";
+  const colorDisplay = useRef(`rgb(${colorRed},${colorGreen},${colorBlue})`);
+
+  useEffect(()=>{
+    colorDisplay.current = `rgb(${colorRed},${colorGreen},${colorBlue})`;
+  })
   
   return (
     <div className="App">
       <h1>Welcome to the RGBer!</h1>
-      <RgbSlider setColor={setColorRed} color="Red" />
-      <RgbSlider setColor={setColorBlue} color="Blue" />
-      <RgbSlider setColor={setColorGreen} color="Green" />
-      <RgbDisplay />
+      <hr />
+      <RgbSlider setColor={setColorRed} color={red} />
+      <p>{colorRed}</p>
+      <hr />
+      <RgbSlider setColor={setColorBlue} color={blue} />
+      <p>{colorBlue}</p>
+      <hr />
+      <RgbSlider setColor={setColorGreen} color={green} />
+      <p>{colorGreen}</p>
+      <hr />
+      <RgbDisplay bgColor={colorDisplay.current} colorRed={colorRed} colorBlue={colorBlue} colorGreen={colorGreen} />
     </div>
   );
 }
